ac4fcd1f892fbc2b0688d1d121d53230b17485aa
Lubomir PANAK <lubomir.panak@ims.co.at>
2023-08-22 10:44:59 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    process: {
-      env: process.env,
-    },
+    'process.env': process.env,
   },
   assetsInclude: ['**/*.md'],
   plugins: [react()],
