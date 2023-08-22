89af57ab774cd33a454e8defb5462cb619f818d5
Lubomir PANAK <lubomir.panak@ims.co.at>
2023-08-22 13:50:14 +0200

    Fix: piwik

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,12 @@ import App from './App';
 import './index.less';
 import { store } from './store.ts';
 
-const {
-  env: { NODE_ENV },
-} = process || {};
-
-NODE_ENV !== 'development'
-  ? PiwikPro.initialize(
+import.meta.env.DEV
+  ? null
+  : PiwikPro.initialize(
       '7062d80e-1b2c-45b1-818b-6dea3457644c',
       'https://drakh.containers.piwik.pro',
-    )
-  : null;
+    );
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
